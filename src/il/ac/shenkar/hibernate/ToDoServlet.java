ee639fb6a22fcadeb3d04f43af9468ff2f09ec79
Arel Gindos <rlgindos@gmail.com>
2017-02-18 12:26:03 +0200

    Huge change, added edit/add/save new tasks

--- src/il/ac/shenkar/hibernate/ToDoServlet.java
+++ src/il/ac/shenkar/hibernate/ToDoServlet.java
@@ -18,9 +18,33 @@ import javax.servlet.http.HttpServletResponse;
 public class ToDoServlet extends HttpServlet {
     HibernateToDoListDAO dao = HibernateToDoListDAO.getInstance();
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher view = request.getRequestDispatcher("index.jsp");
-        view.forward(request, response);
-        request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+        switch(request.getParameter("action")) {
+            case "index":
+                RequestDispatcher view = request.getRequestDispatcher("index.jsp");
+                view.forward(request, response);
+                request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+                break;
+
+            case "delete":
+                int id = Integer.parseInt(request.getParameter("id"));
+                int userID = Integer.parseInt(request.getParameter("userID"));
+                Item item = dao.getItemByID(id);
+                dao.deleteItem(item);
+                request.setAttribute("tasksList", dao.getAllItemsByUserId(userID));//TODO: fix getUserByUserName and add a call to dao
+                request.getRequestDispatcher("/tasks.jsp").forward(request, response);
+                break;
+
+            case "changeStatus":
+                int itemID = Integer.parseInt(request.getParameter("id"));
+                int userID1 = Integer.parseInt(request.getParameter("userID"));
+                Item tempItem = dao.getItemByID(itemID);
+                tempItem.setStatus("done");
+                dao.updateItem(tempItem);
+                request.setAttribute("tasksList", dao.getAllItemsByUserId(userID1));//TODO: fix getUserByUserName and add a call to dao
+                request.getRequestDispatcher("/tasks.jsp").forward(request, response);
+                    break;
+        }
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -41,8 +65,24 @@ public class ToDoServlet extends HttpServlet {
                 request.getRequestDispatcher("/tasks.jsp").forward(request, response);
                 break;
 
-            case "add_task":
-                System.out.println("add_task");
+            case "editTask":
+                int userID = Integer.parseInt(request.getParameter("userID"));
+                Item item = dao.getItemByID(Integer.parseInt(request.getParameter("id")));
+                item.setStatus(request.getParameter("status"));
+                item.setTitle(request.getParameter("title"));
+                dao.updateItem(item);
+                request.setAttribute("tasksList", dao.getAllItemsByUserId(userID));//TODO: fix getUserByUserName and add a call to dao
+                request.getRequestDispatcher("/tasks.jsp").forward(request, response);
+                break;
+            case "addItem":
+                String status = request.getParameter("status");
+                String title = request.getParameter("title");
+                int user_id = Integer.parseInt(request.getParameter("userID"));
+                new Item(user_id,title,status);
+                request.setAttribute("tasksList", dao.getAllItemsByUserId(user_id));//TODO: fix getUserByUserName and add a call to dao
+                request.getRequestDispatcher("/tasks.jsp").forward(request, response);
+                break;
         }
+
     }
 }
