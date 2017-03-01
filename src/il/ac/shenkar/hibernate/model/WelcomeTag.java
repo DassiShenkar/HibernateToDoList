9e89c44cf4a13578b8416aa926f79fdd9eab7c40
Arel Gindos <rlgindos@gmail.com>
2017-03-02 00:00:19 +0200

    Added custom tag

--- src/il/ac/shenkar/hibernate/model/WelcomeTag.java
+++ src/il/ac/shenkar/hibernate/model/WelcomeTag.java
@@ -1,13 +1,24 @@
 package il.ac.shenkar.hibernate.model;
 
 import java.io.IOException;
-
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
+/**
+ * A class that implements custom tags
+ * @see SimpleTagSupport
+ * @author Arel Gindos
+ * @author Dassi Rosen
+ * @author Lior Lerner
+ */
+@SuppressWarnings("ALL")
 public class WelcomeTag extends SimpleTagSupport {
-
+    /**
+     * A method that print to the client the main title
+     * @throws JspException
+     * @throws IOException
+     */
     public void doTag() throws JspException, IOException {
         JspWriter out = getJspContext().getOut();
         out.println("ToDo list");
